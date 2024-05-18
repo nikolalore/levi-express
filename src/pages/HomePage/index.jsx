fcbd57e3d98efe177d8c85bc7d868f1169a9aaab
Nikola Lorenc <nikola.skarkova@gmail.com>
2024-05-18 14:41:59 +0200

    přidání selected seat

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail';
+import { SelectedSeat } from '../../components/SelectedSeat';
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
@@ -13,6 +14,7 @@ export const HomePage = () => {
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange} />
       {journey ? <JourneyDetail journey={journey.stops} /> : null}
+      {journey ? <SelectedSeat number={journey.autoSeat}/> : null}
     </main>
   );
 };
